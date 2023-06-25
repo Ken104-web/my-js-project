e4b41a25fbc4c0f70fd5bb2c01faa05db5344faa
KenCorp <kennethwarui35@gmail.com>
2023-06-25 19:50:02 +0300

    Completed assignment

--- function everyDemeritPoint(speed) {.js
+++ function everyDemeritPoint(speed) {.js
@@ -1,25 +1,22 @@
 function everyDemeritPoint(speed) {
+  let speedLimit = 70;            // Define the speed limit in km/h
+  let kmPerDemerit = 5;           // Define the number of km per demerit point
+  let maxDemerit = 12;            // Define the maximum number of demerit points before license suspension
+  let speedDifference = speed - speedLimit;  // Calculate the speed difference
   
-  let speedLimit = 70;
-  let kmPerDemerit = 5;
-  let maxDemerit = 12;
-  let speedDifference = speed - speedLimit;
-
   if (speed <= speedLimit) {
-    document.getElementById('Speed').textContent = 'Ok';
+    document.getElementById('Speed').textContent = 'Ok';  // If the speed is within the limit, display 'Ok'
     return;
   }
 
-  let demeritPoints = Math.floor(speedDifference / kmPerDemerit);
-
+  let demeritPoints = Math.floor(speedDifference / kmPerDemerit);  // Calculate the demerit points based on the speed difference
+  
   if (demeritPoints > maxDemerit) {
-    document.getElementById('Speed').textContent = 'License suspended'
+    document.getElementById('Speed').textContent = 'License suspended';  // If the demerit points exceed the maximum, display 'License suspended'
   } else {
-    document.getElementById('Speed').textContent = 'Points:' + demeritPoints;
-
+    document.getElementById('Speed').textContent = 'Points:' + demeritPoints;  // Otherwise, display the number of demerit points
   }
-
 }
 
-let driverSpeed = parseInt(prompt("Enter the driver's speed"));
-everyDemeritPoint(driverSpeed);
+let driverSpeed = parseInt(prompt("Enter the driver's speed"));  // Prompt the user to enter the driver's speed
+everyDemeritPoint(driverSpeed);  // Call the function with the entered speed
